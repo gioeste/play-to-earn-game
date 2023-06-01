d400c5ea2d9d2e1f73cc31fd734c6f6fb71de745
gioeste <61016188+gioeste@users.noreply.github.com>
2023-06-01 15:35:07 +0200

    Update _app.tsx

--- application/pages/_app.tsx
+++ application/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const activeChain = "mumbai";
+  const activeChain = "polygon";
 
   return (
     <ThirdwebProvider activeChain={activeChain}>
